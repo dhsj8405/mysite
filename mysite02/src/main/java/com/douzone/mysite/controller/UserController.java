6c455d7dfa1d847165d776d3ca3b3a8300c3faba
dhsj8405 <dhsj8405@naver.com>
2021-10-12 12:18:34 +0900

    user service #1

--- mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -10,14 +10,24 @@ import javax.servlet.http.HttpServletResponse;
 import comd.douzone.web.util.MvcUtil;
 
 
-public class MainController extends HttpServlet {
+public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8"); 			//무조건 넣는게 좋음 데이터 오갈 때 중요
-		MvcUtil.forward("/WEB-INF/views/main/index.jsp", request, response);
+		request.setCharacterEncoding("utf-8");
+		
+		String action = request.getParameter("a");
+		if("joinform".equals(action)) {
+			MvcUtil.forward("/WEB-INF/views/user/joinform.jsp", request, response);	
+		}else if("join".equals(action)) {
+			
+		}else {
+			MvcUtil.redirect("/mysite02",request, response);
+		}
+		
 		
 	}
+
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
