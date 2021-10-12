b91305b06b9ae1785ca2f25f06a557d6184ef2db
dhsj8405 <dhsj8405@naver.com>
2021-10-12 11:24:47 +0900

    #first commit

---
+++ mysite02/src/main/java/com/douzone/mysite/controller/MainController.java
@@ -0,0 +1,26 @@
+package com.douzone.mysite.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class MainController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8"); 			//무조건 넣는게 좋음 데이터 오갈 때 중요
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/main/index.jsp");	//url아니고 경로
+		rd.forward(request, response);
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
