c0399c8281dfc25c7228a2c830e3cdc34fa9f3ec
dhsj8405 <dhsj8405@naver.com>
2021-10-14 00:42:41 +0900

    게시판 리스트페이지로 이동

--- mysite02/src/main/java/com/douzone/mysite/mvc/board/BoardActionFactory.java
+++ mysite02/src/main/java/com/douzone/mysite/mvc/board/BoardActionFactory.java
@@ -1,9 +1,5 @@
 package com.douzone.mysite.mvc.board;
 
-import com.douzone.mysite.mvc.guestbook.AddAction;
-import com.douzone.mysite.mvc.guestbook.DeleteAction;
-import com.douzone.mysite.mvc.guestbook.DeleteFormAction;
-import com.douzone.mysite.mvc.guestbook.ListAction;
 import com.douzone.web.mvc.Action;
 import com.douzone.web.mvc.ActionFactory;
 
@@ -14,12 +10,8 @@ public class BoardActionFactory extends ActionFactory{
 		
 		if("view".equals(actionName)) {
 			action = new AddAction();
-		}else if("modify".equals(actionName)) {
-			action = new DeleteFormAction();
-		}else if("write".equals(actionName)) {
-			action = new DeleteAction();
 		}else {
-			action = new listAction();
+			action = new ListAction();
 		}
 		return action;
 	}
