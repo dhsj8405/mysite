6216b709c321140ef566e33436a24de89b2237c7
dhsj8405 <dhsj8405@naver.com>
2021-10-21 20:18:30 +0900

    갤러리만드는중

--- mysite03/src/main/java/com/douzone/mysite/controller/GalleryController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/GalleryController.java
@@ -1,15 +1,40 @@
 package com.douzone.mysite.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.douzone.mysite.service.GalleryService;
+import com.douzone.mysite.vo.GuestbookVo;
 
 @Controller
 @RequestMapping("/gallery")
 public class GalleryController {
+	@Autowired
+	private GalleryService galleryService;
 	
 	@RequestMapping("")
 	public String index() {
 		
 		return "gallery/index";
 	}
+	@RequestMapping(value="/upload", method=RequestMethod.POST)
+	public String upload(
+			@RequestParam (value = "comments", required= true, defaultValue = "") String comments,
+			@RequestParam(value="file") MultipartFile multipartFile,
+			Model model) {
+			System.out.println("comments:"+comments);
+			String url = galleryService.restore(multipartFile);
+			System.out.println(url);
+			model.addAttribute("url",url);
+			return "redirect:/gallery";
+		}
+//	List<GuestbookVo> list = guestbookService.getMessageList();
+//	model.addAttribute("list", list);
 }
