8ec5cb5468111dee86ae86ce0474332fa882efd9
dhsj8405 <dhsj8405@naver.com>
2021-10-25 13:10:30 +0900

    ajax #1

--- mysite03/src/main/java/com/douzone/mysite/controller/MainController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/MainController.java
@@ -1,9 +1,9 @@
 package com.douzone.mysite.controller;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.douzone.mysite.security.Auth;
 
 @Controller
 public class MainController {
@@ -13,5 +13,11 @@ public class MainController {
 	@RequestMapping({"","/main"})
 	public String index() {
 		return "main/index";
+	}
+	
+	@RequestMapping("/hello")
+	public void message(HttpServletResponse resp) throws Exception{
+		resp.setContentType("application/json; charset=UTF-8");
+		resp.getWriter().print("{\"message\":\"Hello World\"}");
 	}
 }
--- mysite03/src/main/java/com/douzone/mysite/service/BoardService.java
+++ mysite03/src/main/java/com/douzone/mysite/service/BoardService.java
@@ -53,9 +53,9 @@ public class BoardService {
 	public Map<String, Object> getContentsList( int currentPage, String keyword ){
 		
 		//1. 페이징을 위한 기본 데이터 계산
-		int totalCount = boardRepository.getTotalCount( keyword ); 
-		int pageCount = (int)Math.ceil( (double)totalCount / LIST_SIZE );
-		int blockCount = (int)Math.ceil( (double)pageCount / PAGE_SIZE );
+		int totalCount = boardRepository.getTotalCount( keyword ); 	//총 게시글 수
+		int pageCount = (int)Math.ceil( (double)totalCount / LIST_SIZE ); // 게시글 5개씩 출력시 총 페이지 수 
+		int blockCount = (int)Math.ceil( (double)pageCount / PAGE_SIZE );	// 페이지 5개가 1블럭(화살표로 넘기기위해 필요)
 		int currentBlock = (int)Math.ceil( (double)currentPage / PAGE_SIZE );
 		
 		//2. 파라미터 page 값  검증 : 마지막 페이지보다 높은 페이지값이 파라미터로 들어오면 마지막 페이지로 세팅
@@ -70,10 +70,10 @@ public class BoardService {
 		}
 		
 		//3. view에서 페이지 리스트를 렌더링 하기위한 데이터 값 계산
-		int beginPage = currentBlock == 0 ? 1 : (currentBlock - 1) * PAGE_SIZE + 1;
-		int prevPage = ( currentBlock > 1 ) ? ( currentBlock - 1 ) * PAGE_SIZE : 0;
-		int nextPage = ( currentBlock < blockCount ) ? currentBlock * PAGE_SIZE + 1 : 0;
-		int endPage = ( nextPage > 0 ) ? ( beginPage - 1 ) + LIST_SIZE : pageCount;
+		int beginPage = currentBlock == 0 ? 1 : (currentBlock - 1) * PAGE_SIZE + 1; 	// 1, 6, 11 	: 페이징 맨왼쪽 출력되는 페이지
+		int prevPage = ( currentBlock > 1 ) ? ( currentBlock - 1 ) * PAGE_SIZE : 0;		//    5, 10, 	: 왼쪽 화살표 눌렸을때 나오는 페이지  
+		int nextPage = ( currentBlock < blockCount ) ? currentBlock * PAGE_SIZE + 1 : 0; //6, 11, 16 	:오른쪽 화살표 눌렸을 때 나오는 페이지
+		int endPage = ( nextPage > 0 ) ? ( beginPage - 1 ) + LIST_SIZE : pageCount;		// 5, 10, 15 : 페이징 맨 오른쪽에 출력되는 페이지 
 		
 		//4. 리스트 가져오기
 		List<BoardVo> list = boardRepository.findAllByPageAndKeword( keyword, currentPage, LIST_SIZE );
