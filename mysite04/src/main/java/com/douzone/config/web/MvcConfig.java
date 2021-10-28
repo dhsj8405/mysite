d99dacef690a4d5ab6fe3be509d00d3cfb187f20
dhsj8405 <dhsj8405@naver.com>
2021-10-28 16:11:41 +0900

    WebConfig

--- mysite04/src/main/java/com/douzone/config/web/MvcConfig.java
+++ mysite04/src/main/java/com/douzone/config/web/MvcConfig.java
@@ -2,6 +2,7 @@ package com.douzone.config.web;
 
 
 import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,8 +11,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -21,7 +24,7 @@ import org.springframework.web.servlet.view.JstlView;
 @EnableWebMvc
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
-	// viewREsolver
+	// viewResolver
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -45,7 +48,21 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 	
 	@Bean
 	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
-		return null;
+		Jackson2ObjectMapperBuilder builder 
+			= new Jackson2ObjectMapperBuilder()
+			.indentOutput(true)
+			.dateFormat(new SimpleDateFormat("yyyy-mm-dd"));
+		
+		
+		MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter(builder.build()); 
+		messageConverter.setSupportedMediaTypes(
+				Arrays.asList(
+						new MediaType("application","json",Charset.forName("utf-8"))					
+						)
+				);
+		
+		
+		return messageConverter;
 	}
 	
 	@Override
@@ -54,5 +71,14 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 		converters.add(mappingJackson2HttpMessageConverter());
 		
 	}
+
+	// Default Servlet Handler
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	
+
+	
 		
 }
