1a41a6709638a822ababcb25180b8711fb2112c4
dhsj8405 <dhsj8405@naver.com>
2021-11-02 10:29:41 +0900

    mysite06 # yml,BootInitializer

--- mysite06/src/main/java/com/douzone/mysite/MySiteApplication.java
+++ mysite06/src/main/java/com/douzone/mysite/MySiteApplication.java
@@ -1,3 +1,12 @@
 package com.douzone.mysite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@SpringBootConfiguration + @ComponentScan + @EnableAutoConfiguration 
 public class MySiteApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MySiteApplication.class, args);
+	}
 }
